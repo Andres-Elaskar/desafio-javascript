7b950024543b273a0b5e11580843fdfe6af922f2
Andy Elaskar <andres_elaskar@hotmail.com>
2022-08-02 21:10:01 -0300

    mas cambios

--- js/main.js
+++ js/main.js
@@ -39,11 +39,12 @@ this.precio = precioParametro;
 }
 const producto0252 = new Producto("Almendras Non Pareil", 15, 3500);
 const producto0161 = new Producto("Nueces con Cascara", 8, 1285);
-const producto0043 = new Producto("Tomates Secos",0, 
-2650);
-const producto0025 = new Producto("Arandanos Rojos", 
-0, 45200);
-const producto0001 = new Producto("Ciruela President Grande", 125, 450);
+const producto0165 = new Producto("Nueces Peladas Mariposa", 25, 4250);
+const producto0043 = new Producto("Tomates Secos", 0, 2650);
+const producto0025 = new Producto("Arandanos Rojos", 0, 45200);
+const producto0075 = new Producto("Ciruelas President", 125, 450);
+const producto0001 = new Producto("Ciruelas D'agen sin carozo", 25, 4250);
+const producto0140 = new Producto("Peras Willams", 12, 3250);
 
 //-------------Arrays------------//
 
@@ -65,6 +66,12 @@ agregarAlCarrito("Almendas Non Pareil",);
 
 //--------------Funciones de Orden Superior--------------//
 
+const agregarAlCarrito = ['Almendras Non Pareil',
+];
+const total = agregarAlCarrito.reduce((acumulador,
+elemento) => acumulador + elemento, 0);console.log
+(total);
+
 function borrarProductoDelCarrito(idDelproducto) {
 const index = carrito.findIndex((producto) => 
 producto.id === idDelproducto);
@@ -75,8 +82,27 @@ console.log(carrito);
 
 borrarProductoDelCarrito(0043);
 
-//const agregarAlCarrito = ['Almendras Non Pareil',
-//];
-//const total = agregarAlCarrito.reduce((acumulador,
-//elemento) => acumulador + elemento, 0);console.log
-//(total);
+//------------DOM-------------//
+
+//Metodo 2 - por Clase - trae un array de objetos
+<ul>
+    <li class='producto'>Almendras Non Pareil</li>
+    <li class='producto'>Nueces con Cascara</li>
+    <li class='producto'>Nueces Peladas Mariposa</li>
+    <li class='producto'>Tomates Secos</li>
+    <li class='producto'>Arandanos Rojos</li>
+    <li class='producto'>Ciruelas President</li>
+    <li class='producto'>Ciruelas D'agen sin carozo</li>
+    <li class='producto'>Peras Williams</li>
+</ul>
+let producto = document.getElementsByClassName('producto');
+console.log(producto[0].innerHTML);
+console.log(producto[1].innerHTML);
+console.log(producto[2].innerHTML);
+console.log(producto[3].innerHTML);
+console.log(producto[4].innerHTML);
+console.log(producto[5].innerHTML);
+console.log(producto[6].innerHTML);
+console.log(producto[7].innerHTML);
+
+
